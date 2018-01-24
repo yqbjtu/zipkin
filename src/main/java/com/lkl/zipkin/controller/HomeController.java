dfbb5c315a36ae97f5b90f08a70921d62b99ac14
yqbjtu <yqbjtu@163.com>
2018-01-24 14:58:59 +0800

    upgrade springboot and brave
    Change-Id: If45ec648a0cfac7111ecb8ba38f1483fd70bcfd7

--- src/main/java/com/lkl/zipkin/controller/HomeController.java
+++ src/main/java/com/lkl/zipkin/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.lkl.zipkin.controller;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,6 +34,14 @@ public class HomeController {
         return " [service1 sleep " + sleep+" ms]" + response.body().toString();
     }
 
+    @RequestMapping("start2")
+    public String start2() throws InterruptedException, IOException {
+        int sleep= random.nextInt(100);
+        TimeUnit.MILLISECONDS.sleep(sleep);
+        Request request = new Request.Builder().url("http://localhost:9092/tar2").get().build();
+        Response response = client.newCall(request).execute();
+        return " [service1 start2  sleep " + sleep+" ms], it call service4 tar2" + response.body().toString();
+    }
 
     @RequestMapping("foo")
     public String foo() throws InterruptedException, IOException {
@@ -47,6 +56,17 @@ public class HomeController {
         return " [service2 sleep " + sleep+" ms]" + result;
     }
 
+    /*
+     * this method is used to call zipkin python demo by its api
+     */
+    @RequestMapping("python")
+    public String pythonService() throws InterruptedException, IOException {
+        int sleep= random.nextInt(100);
+        TimeUnit.MILLISECONDS.sleep(sleep);
+        Request request = new Request.Builder().url("http://localhost:9093/api").get().build();
+        Response response = client.newCall(request).execute();
+        return " [service  sleep " + sleep+" ms], it will call python service http://localhost:9092/api:" + response.body().toString();
+    }
 
     @RequestMapping("bar")
     public String bar() throws InterruptedException, IOException {  //service3 method
@@ -62,6 +82,14 @@ public class HomeController {
         return " [service4 sleep " + sleep+" ms]";
     }
 
-
+    /*
+     * service1 will call service4 directly by http://localhost:9092/tar2
+     */
+    @RequestMapping("tar2")
+    public String tar2() throws InterruptedException, IOException { //service4 method
+        int sleep= random.nextInt(100);
+        TimeUnit.MILLISECONDS.sleep(sleep);
+        return " [service4 sleep " + sleep+" ms]";
+    }
 
 }
